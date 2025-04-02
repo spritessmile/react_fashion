19bfdeb96d995c1eeb2168d8ea40846b899eaccf
spritessmile <adam.azim307@gmail.com>
2025-04-02 09:12:36 +0900

    chore(style): tiny style modify

--- src/components/ui/navbar-menu.tsx
+++ src/components/ui/navbar-menu.tsx
@@ -70,7 +70,7 @@ export const Menu = ({
   return (
     <nav
       onMouseLeave={() => setActive(null)} // resets the state
-      className="shadow-input relative flex items-center justify-center space-x-4 rounded-full border border-transparent bg-white px-8 py-6 dark:border-white/[0.2] dark:bg-black"
+      className="shadow-input relative flex items-center justify-center space-x-4 bg-white px-8 py-6 dark:border-white/[0.2] dark:bg-black"
     >
       {children}
     </nav>
