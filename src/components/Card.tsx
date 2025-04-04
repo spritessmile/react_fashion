fa7b41d20265bb4ee46bc5fa4b177e6c24b398ae
spritessmile <adam.azim307@gmail.com>
2025-04-04 12:21:22 +0900

    fix: innocent transparent card background

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,5 @@
 const Card = ({ children }: { children: React.ReactNode }) => (
-  <div className="flex w-full items-center justify-center p-3 shadow">
+  <div className="flex w-full items-center justify-center rounded-full border bg-white p-5 shadow dark:border-2 dark:bg-gray-900">
     {children}
   </div>
 )
