3528e90780d89a0c071b7484261c6b3716b9979d
spritessmile <adam.azim307@gmail.com>
2025-04-08 15:11:41 +0900

    feat: add Contact section on home page

---
+++ src/components/Contact.tsx
@@ -0,0 +1,113 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Textarea } from '@/components/ui/textarea'
+import { Input } from '@/components/ui/input'
+
+const FormSchema = z.object({
+  username: z.string().min(2, {
+    message: 'Username must be at least 2 characters.',
+  }),
+  emailAddress: z.string().min(2, {
+    message: 'Username must be at least 2 characters.',
+  }),
+  contactContent: z.string().min(2, {
+    message: 'Username must be at least 2 characters.',
+  }),
+})
+
+const onSubmit = (values: z.infer<typeof FormSchema>) => {}
+
+const Contact = () => {
+  export function InputForm() {
+    const form = useForm<z.infer<typeof FormSchema>>({
+      resolver: zodResolver(FormSchema),
+      defaultValues: {
+        username: '',
+      },
+    })
+
+    function onSubmit(data: z.infer<typeof FormSchema>) {
+      toast({
+        title: 'You submitted the following values:',
+        description: (
+          <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+            <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+          </pre>
+        ),
+      })
+    }
+    // ...
+    return (
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>お名前</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormDescription>
+                  連絡時の名前を教えて頂いております
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="emailAddress"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>メールアドレス</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormDescription>
+                  担当者はこのメールアドレスでご連絡させていただきます
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="contactContent"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>お問い合わせ</FormLabel>
+                <FormControl>
+                  <Textarea className="resize-none" {...field} />
+                </FormControl>
+                <FormDescription>
+                  連絡、または契約について気になることを気軽にお問い合わせください。
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    )
+  }
+}
+export default Contact
