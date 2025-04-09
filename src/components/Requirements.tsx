d74cd4f1af206134316f333f6e51a785104460ae
spritessmile <adam.azim307@gmail.com>
2025-04-09 21:13:47 +0900

    chore: fill data

--- src/components/Requirements.tsx
+++ src/components/Requirements.tsx
@@ -11,14 +11,34 @@ import ThreeDCard from '@/components/ThreeDCard'
 const Requirements = () => {
   return (
     <div className="flex flex-wrap items-center justify-around gap-4">
-      <ThreeDCard icon={<ChartNoAxesGantt size={64} />} desc={'af'} />
-
-      <ChartNoAxesGantt size={64} />
-      <ShieldCheck size={64} />
-      <Scale size={64} />
-      <Fan size={64} />
-      <House size={64} />
-      <History size={64} />
+      <ThreeDCard
+        icon={<ChartNoAxesGantt size={64} />}
+        title="管理スタイル"
+        desc={'週五回程度巡回'}
+      />
+      <ThreeDCard
+        icon={<ShieldCheck size={64} />}
+        title="清掃"
+        desc={'管理人週1回程度；共用部分のゴミ出しは入居者当番'}
+      />
+      <ThreeDCard
+        icon={<Scale size={64} />}
+        title="ルール"
+        desc={
+          '喫煙は喫煙所のみ可；友人の宿泊は要事前連絡；共用部のゴミ出しは住人当番制；3日おきのローテーション'
+        }
+      />
+      <ThreeDCard icon={<Fan size={64} />} title="イベント" desc={'なし'} />
+      <ThreeDCard
+        icon={<House size={64} />}
+        title="コミュニティ"
+        desc={'アットホーム - みんなで仲良く'}
+      />
+      <ThreeDCard
+        icon={<History size={64} />}
+        title="入居期間"
+        desc={'長期可；短期の1か月からも可'}
+      />
     </div>
   )
 }
