5bb9f9a39b6937b993a59e5abd1351c759668497
spritessmile <adam.azim307@gmail.com>
2025-04-09 21:13:29 +0900

    chore: add `title` prop

--- src/components/ThreeDCard.tsx
+++ src/components/ThreeDCard.tsx
@@ -11,21 +11,21 @@ import { LucideProps } from 'lucide-react'
 
 interface ThreeDCardProps {
   icon: ReactNode
+  title: string
   desc: string
 }
 
 const ThreeDCard = (card: ThreeDCardProps) => {
   return (
     <CardContainer className="inter-var">
-      <CardBody className="group/card relative h-auto w-auto rounded-xl border border-black/[0.1] bg-gray-50 p-6 sm:w-[30rem] dark:border-white/[0.2] dark:bg-black dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1]">
-        <CardItem translateZ="100" className="mt-4 w-full">
-          {card.icon}
+      <CardBody className="group/card relative rounded-xl border border-black/[0.1] bg-gray-50 p-5 dark:border-white/[0.2] dark:bg-black dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1]">
+        <CardItem translateZ="100">{card.icon}</CardItem>
+
+        <CardItem translateZ="50">
+          <h2 className="text-center text-xl font-semibold">{card.title}</h2>
         </CardItem>
 
-        <CardItem
-          translateZ="50"
-          className="text-xl font-semibold text-neutral-600 dark:text-white"
-        >
+        <CardItem translateZ="50" className="text-neutral-600">
           {card.desc}
         </CardItem>
       </CardBody>
