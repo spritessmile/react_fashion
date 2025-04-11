dd86f8992dcd23c767284f9149611844549f160e
spritessmile <adam.azim307@gmail.com>
2025-04-11 19:22:15 +0900

    chore: add some margin to top

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import locationImg from '@/assets/images/location.png'
 
 const Location = () => (
-  <div className="flex items-center justify-center">
+  <div className="mb-20 flex items-center justify-center">
     <Image
       src={locationImg}
       className="m-5 w-fit rounded-xl"
