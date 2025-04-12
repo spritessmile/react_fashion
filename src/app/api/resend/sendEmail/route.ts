7cfc8514922e492477f5905f3b4d237ff338bd80
spritessmile <adam.azim307@gmail.com>
2025-04-12 21:22:13 +0900

    fix: server can not accept data from client

--- src/app/api/resend/sendEmail/route.ts
+++ src/app/api/resend/sendEmail/route.ts
@@ -1,24 +1,36 @@
 import { Resend } from 'resend'
 import { EmailTemplate } from '@/components/EmailTemplate'
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextRequest, NextResponse } from 'next/server'
 
 const resend = new Resend('re_G8Uia7rS_9J4d8z7p7ZMCn1H8jsNwbxeE')
 
-export async function POST() {
-  try {
-    const { data, error } = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: ['gaomingzhao666@outlook.com'],
-      subject: 'Hello world',
-      react: EmailTemplate({ firstName: '' }) as React.ReactElement,
-    })
+interface ResponseData {
+  username: string
+  emailAddress: string
+  contactContent: string
+}
 
-    if (error) {
-      return Response.json({ error }, { status: 500 })
-    }
+export async function POST(req: NextRequest, res: NextResponse<ResponseData>) {
+  const { username, emailAddress, contactContent } = await req.json()
 
-    return Response.json(data)
-  } catch (error) {
+  const { data, error } = await resend.emails.send({
+    from: '星野民家シェアハウス <onboarding@resend.dev>',
+    to: ['gaomingzhao666@outlook.com'],
+    subject: '問い合わせ',
+    react: EmailTemplate({
+      username: username,
+      emailAddress: emailAddress,
+      contactContent: contactContent,
+    }) as React.ReactElement,
+  })
+
+  console.log({ username, emailAddress, contactContent })
+
+  res.json({ username: username, emailAddress, contactContent })
+
+  if (error) {
     return Response.json({ error }, { status: 500 })
   }
+
+  return Response.json(data)
 }
