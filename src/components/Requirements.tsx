5c7e3feb056f53e5f0cd5b11d0f79cbc6fe118e2
spritessmile <adam.azim307@gmail.com>
2025-04-14 11:49:43 +0900

    chore: add title for each section

--- src/components/Requirements.tsx
+++ src/components/Requirements.tsx
@@ -7,38 +7,44 @@ import {
   History,
 } from 'lucide-react'
 import ThreeDCard from '@/components/ThreeDCard'
+import { div } from 'framer-motion/client'
 
 const Requirements = () => {
   return (
-    <div className="grid grid-cols-1 gap-10 p-20 md:grid-cols-2 lg:grid-cols-3">
-      <ThreeDCard
-        icon={<ChartNoAxesGantt size={64} />}
-        title="管理スタイル"
-        desc={'週五回程度巡回'}
-      />
-      <ThreeDCard
-        icon={<ShieldCheck size={64} />}
-        title="清掃"
-        desc={'管理人週1回程度；共用部分のゴミ出しは入居者当番'}
-      />
-      <ThreeDCard
-        icon={<Scale size={64} />}
-        title="ルール"
-        desc={
-          '喫煙は喫煙所のみ可；友人の宿泊は要事前連絡；共用部のゴミ出しは住人当番制；3日おきのローテーション'
-        }
-      />
-      <ThreeDCard icon={<Fan size={64} />} title="イベント" desc={'なし'} />
-      <ThreeDCard
-        icon={<House size={64} />}
-        title="コミュニティ"
-        desc={'アットホーム - みんなで仲良く'}
-      />
-      <ThreeDCard
-        icon={<History size={64} />}
-        title="入居期間"
-        desc={'長期可；短期の1か月からも可'}
-      />
+    <div className="mb-20">
+      <h2 className="mb-10 text-center text-3xl font-semibold">
+        マネージメント ルール
+      </h2>
+      <div className="grid grid-cols-1 gap-10 px-20 md:grid-cols-2 lg:grid-cols-3">
+        <ThreeDCard
+          icon={<ChartNoAxesGantt size={64} />}
+          title="管理スタイル"
+          desc={'週五回程度巡回'}
+        />
+        <ThreeDCard
+          icon={<ShieldCheck size={64} />}
+          title="清掃"
+          desc={'管理人週1回程度；共用部分のゴミ出しは入居者当番'}
+        />
+        <ThreeDCard
+          icon={<Scale size={64} />}
+          title="ルール"
+          desc={
+            '喫煙は喫煙所のみ可；友人の宿泊は要事前連絡；共用部のゴミ出しは住人当番制；3日おきのローテーション'
+          }
+        />
+        <ThreeDCard icon={<Fan size={64} />} title="イベント" desc={'あり'} />
+        <ThreeDCard
+          icon={<House size={64} />}
+          title="コミュニティ"
+          desc={'アットホーム - みんなで仲良く'}
+        />
+        <ThreeDCard
+          icon={<History size={64} />}
+          title="入居期間"
+          desc={'長期可；短期の1か月からも可'}
+        />
+      </div>
     </div>
   )
 }
