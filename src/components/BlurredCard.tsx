dd89b70e99aa16a3b473f399e201e63e38cfe781
spritessmile <adam.azim307@gmail.com>
2025-04-14 12:02:45 +0900

    chore: style optimization

--- src/components/BlurredCard.tsx
+++ src/components/BlurredCard.tsx
@@ -1,5 +1,5 @@
 const Card = ({ children }: { children: React.ReactNode }) => (
-  <div className="flex w-full items-center justify-center rounded-full border p-5 shadow backdrop-blur-sm dark:border-2">
+  <div className="m-5 flex items-center justify-center rounded-full border p-4 shadow backdrop-blur-sm dark:border-2">
     {children}
   </div>
 )
